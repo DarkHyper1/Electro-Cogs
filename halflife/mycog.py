4e44f05955fd2c9c545cd20342ea198ddb8dad55
BuildTools <unconfigured@null.spigotmc.org>
2017-09-07 07:59:07 -0400

    Added Cogs

---
+++ halflife/mycog.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+
+class halflife:
+    """If Someone Says Half-Life, They Must Be Punished"""
+    
+    def __init__(self, bot):
+        self.bot = bot
+        self.number = 2017
+    @commands.command()
+    async def halflife(self):
+        """Punish a User for Saying Half-Life"""
+        
+        #Your code will go here
+        self.number = self.number + 1
+        await self.bot.say("You have mentioned Half-Life 3 or a game set in the Half-Life Universe, and as such have delayed the game by a year.")
+        await self.bot.say("The game will now be released on January 18, {}".format(self.number))
+
+def setup(bot):
+    bot.add_cog(halflife(bot))
