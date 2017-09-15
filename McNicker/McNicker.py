e58bc15a5b9a3f0e90b67f704a24f8f096597ed2
Electromaster232 <25728719+Electromaster232@users.noreply.github.com>
2017-09-15 18:35:00 -0400

    Update McNicker.py

--- McNicker/McNicker.py
+++ McNicker/McNicker.py
@@ -12,7 +12,7 @@ class McNicker:
         """Enter a Nickname. It will be changed In-game."""
 
         #Your code will go here
-        await self.bot.say(user.mention + " Has updated " + playername "s Nickname!)
+        await self.bot.say(user.mention + " Has updated " + playername + "s Nickname!)
         await self.bot.say("Nick Changed!")
         message = "nick {} {}".format(playername, nickname)
         await self.bot.send_message(self.bot.get_channel('320638374551748609'), "nick {} {}".format(playername, nickname))
