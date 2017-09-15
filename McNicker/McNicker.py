51da91179bd2af46aa6f3a638989fbffe5aaf9ba
Electromaster232 <25728719+Electromaster232@users.noreply.github.com>
2017-09-15 18:26:45 -0400

    Update McNicker.py

--- McNicker/McNicker.py
+++ McNicker/McNicker.py
@@ -8,10 +8,11 @@ class McNicker:
         self.bot = bot
 
     @commands.command()
-    async def McNick(self, playername, nickname):
+    async def McNick(self, playername, nickname, user : discord.Member):
         """Enter a Nickname. It will be changed In-game."""
 
         #Your code will go here
+        await self.bot.say(user.mention + " Has updated " + playername "'s Nickname!)
         await self.bot.say("Nick Changed!")
         message = "nick {} {}".format(playername, nickname)
         await self.bot.send_message(self.bot.get_channel('320638374551748609'), "nick {} {}".format(playername, nickname))
