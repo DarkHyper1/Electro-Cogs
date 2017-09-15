a452e34b327b00452cd4aec8ff74214d0148e5ca
Electromaster232 <25728719+Electromaster232@users.noreply.github.com>
2017-09-15 18:01:51 -0400

    Add files via upload

---
+++ McNicker/NickCog.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+
+class McNicker:
+    """My custom cog that does stuff!"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def McNick(self, playername, nickname):
+        """Enter a Nickname. It will be changed In-game."""
+
+        #Your code will go here
+        await self.bot.say("Nick Changed!")
+        message = "nick {} {}".format(playername, nickname)
+        self.bot.send_message(self.bot.get_channel('320638374551748609'), "nick {} {}".format(playername, nickname))
+
+def setup(bot):
+    bot.add_cog(McNicker(bot))
