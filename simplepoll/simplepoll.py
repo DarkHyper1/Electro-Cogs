26db059a9d3ca1b90694ab2e8cccc09e00594e64
Electromaster232 <25728719+Electromaster232@users.noreply.github.com>
2017-09-21 21:36:17 -0400

    Update simplepoll.py

--- simplepoll/simplepoll.py
+++ simplepoll/simplepoll.py
@@ -16,11 +16,12 @@ class simplepoll:
         await self.bot.add_reaction(message,"\N{REGIONAL INDICATOR SYMBOL LETTER N}")
         await self.bot.add_reaction(message,"\N{REGIONAL INDICATOR SYMBOL LETTER Y}")
         
-    @commands.command()    
-    async def endpoll(self, pollid):
+    @commands.command(pass_context=True)    
+    async def endpoll(self, ctx, pollid):
         """Ends a poll"""
         #les do this
-        message = await self.bot.get_message(pollid)
+        channel = ctx.message.channel
+        message = await self.bot.get_message(channel, pollid)
         reaction = discord.utils.get(message.reactions, emoji="\N{REGIONAL INDICATOR SYMBOL LETTER N}")
         reaction2 = discord.utils.get(message.reactions, emoji="\N{REGIONAL INDICATOR SYMBOL LETTER Y}")
         react1count = self.reaction.count(reaction)
