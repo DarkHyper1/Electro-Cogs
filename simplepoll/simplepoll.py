693995cc237219423f72b8c3d5606924e401545c
Electromaster232 <25728719+Electromaster232@users.noreply.github.com>
2017-09-21 21:28:30 -0400

    Update simplepoll.py

--- simplepoll/simplepoll.py
+++ simplepoll/simplepoll.py
@@ -8,12 +8,27 @@ class simplepoll:
         self.bot = bot
 
     @commands.command()
-    async def startpoll(self, question):
+    async def startpoll(self, *, question):
         """Starts a poll"""
 
         #Your code will go here
         message = await self.bot.say("POLL: {}".format(question))
         await self.bot.add_reaction(message,"\N{REGIONAL INDICATOR SYMBOL LETTER N}")
+        await self.bot.add_reaction(message,"\N{REGIONAL INDICATOR SYMBOL LETTER Y}")
+        
+    @commands.command()    
+    async def endpoll(self, pollid):
+        """Ends a poll"""
+        #les do this
+        message = await bot.get_message(pollid)
+        reaction = discord.utils.get(message.reactions, emoji="\N{REGIONAL INDICATOR SYMBOL LETTER N}")
+        reaction2 = discord.utils.get(message.reactions, emoji="\N{REGIONAL INDICATOR SYMBOL LETTER Y}")
+        react1count = self.reaction.count(reaction)
+        react2count = self.reaction.count(reaction2)
+        if (reaction1 > reaction2):
+            await self.bot.say("The Poll has ended! The users have chosen NO!")
+        else:
+            await self.bot.say("The Poll has ended! The users have chosen YES!")
 
 def setup(bot):
     bot.add_cog(simplepoll(bot))
