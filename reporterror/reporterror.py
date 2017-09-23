832aca6450ffbd391e93c2fe513b42fa481a6797
Electromaster232 <25728719+Electromaster232@users.noreply.github.com>
2017-09-23 12:27:43 -0400

    Update reporterror.py

--- reporterror/reporterror.py
+++ reporterror/reporterror.py
@@ -1,18 +1,27 @@
 import discord
 from discord.ext import commands
+from raven import Client
+from cogs.utils import checks
 
-class Mycog:
-    """My custom cog that does stuff!"""
+class reporterror:
+    """A Cog to report errors to me. You may not use without permission (it wont work!)!"""
 
     def __init__(self, bot):
         self.bot = bot
-
-    @commands.command()
-    async def mycom(self):
+        
+        
+    @checks.mod()
+    @commands.command(pass_context=True)
+    async def reporterror(self, ctx, *, errormessage):
         """This does stuff!"""
-
+        client = Client('')
+        channel = ctx.message.channel
+        sender = ctx.message.author.display_name
+        server = ctx.message.server.name
         #Your code will go here
-        await self.bot.say("I can do stuff!")
+        await self.bot.say("Processing Report")
+        client.captureMessage("User " + sender + " Has sent an error from the server " + server + ", in the channel " + channel + ". Their message: " + errormessage)
+        await self.bot.say("Message Sent!")
 
 def setup(bot):
-    bot.add_cog(Mycog(bot))
+    bot.add_cog(reporterror(bot))
