b217ada42918cf32e7dc03d2948a3325ac788de0
Electromaster232 <25728719+Electromaster232@users.noreply.github.com>
2017-09-27 21:08:30 -0400

    Update ugreport.py

--- ugreport/ugreport.py
+++ ugreport/ugreport.py
@@ -8,14 +8,14 @@ class UGReport:
         self.bot = bot
 
     @commands.command(pass_context=True)
-    async def report(self, player, *, reason):
+    async def report(self, ctx, player, *, reason):
         """Report a user. Please provide evidence."""
 
 
         
-        
+        author = ctx.message.author
         await self.bot.say("Your report againist " + player + " Has been created.")
-        await self.bot.send_message(self.bot.get_channel('361734544971268109'), "A Report has been filed! \n User: " + player + "\n Reason: " + reason)
+        await self.bot.send_message(self.bot.get_channel('361734544971268109'), "A Report has been filed! \n User: " + player + "\n Reason: " + reason + "\n Reported by: " + author)
 
 def setup(bot):
     bot.add_cog(UGReport(bot))
