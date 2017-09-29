a08eaff1165f7a6b16e9996fff55df6a53b21fb4
Electromaster232 <25728719+Electromaster232@users.noreply.github.com>
2017-09-29 18:49:44 -0400

    Update mcrecipe.py

--- mcrecipe/mcrecipe.py
+++ mcrecipe/mcrecipe.py
@@ -8,18 +8,18 @@ from discord.ext import commands
 from cogs.utils import checks
 
 class mcrecipe:
-    """Dem Gosh Darn Dank Memes"""
+    """Minecraft Recipe Control System"""
 
     def __init__(self, bot):
         self.bot = bot
    
 
     @commands.command(pass_context=True)
-    async def getrecipe(self, ctx, *, item):
+    async def getrecipe(self, ctx, item):
         """Send a recipe in chat"""
 
 
-
+        
         db = MySQLdb.connect(host="mysql.theendlessweb.com",    # your host, usually localhost
                      user="electrom_dankmemesuser",         # your username
                      passwd="DankMemes",  # your password
@@ -27,5 +27,23 @@ class mcrecipe:
         cur = db.cursor()
       
         db.close()
+        
+        
+        
+    @commands.command()
+    async def addrecipe(self, item, piclink):
+        """Adds a recipe"""
+        
+         db = MySQLdb.connect(host="mysql.theendlessweb.com",    # your host, usually localhost
+                     user="electrom_dankmemesuser",         # your username
+                     passwd="DankMemes",  # your password
+                     db="electrom_dankmemes")
+        cur = db.cursor()
+        cur.execute("INSERT INTO recipes {item, itemlink} VALUES (\"{}\", \"{}\")".format(item, piclink))        
+        db.commit()
+        await self.bot.say("Added")
+        db.close()
+        
+   
 def setup(bot):
     bot.add_cog(mcrecipe(bot))
