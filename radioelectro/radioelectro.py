06b400a6b83930b80ca781c2c85628d10ef54e73
Electromaster232 <25728719+Electromaster232@users.noreply.github.com>
2017-10-25 09:48:45 -0400

    Lets see if this works...

--- radioelectro/radioelectro.py
+++ radioelectro/radioelectro.py
@@ -12,16 +12,16 @@ class RadioHaru:
         self.bot = bot
     
     @commands.group(pass_context=True, no_pm=True)
-    async def radioharu(self, ctx):
+    async def electroradio(self, ctx):
         """Radio Haru"""
         if ctx.invoked_subcommand is None:
             await send_cmd_help(ctx)
-            await self.bot.say("https://radioharu.pw/")
+            await self.bot.say("https://theendlessweb.com")
 
     @radioharu.command(pass_context=True, no_pm=True)
     @checks.serverowner_or_permissions(manage_server=True)
     async def play(self, ctx):
-        """Play Radio Haru"""
+        """StartElectroRadio"""
         server = ctx.message.server
         author = ctx.message.author
         if self.voice_connected(server):
@@ -31,13 +31,12 @@ class RadioHaru:
             voice = await self.bot.join_voice_channel(voice_channel)
             Channel = ctx.message.channel
             await self.bot.send_typing(Channel)
-            player = voice.create_ffmpeg_player('https://cdn.discordapp.com/attachments/336598653923753987/360413654224601089/Radio-Haru.ogg')
             player.start()
             await asyncio.sleep(7)
             player.stop()
-            player = voice.create_ffmpeg_player('https://stream.radioharu.pw/owo')
+            player = voice.create_ffmpeg_player('http://play.theendlessweb.com:8000/listen.pls')
             player.start()
-            await self.bot.say(":green_heart: **Playing Radio Haru!**")
+            await self.bot.say(":green_heart: **Here we go! Joining Voice!**")
             
     @radioharu.command(pass_context=True, no_pm=True)
     @checks.serverowner_or_permissions(manage_server=True)
@@ -61,7 +60,7 @@ class RadioHaru:
             radio = True
             while radio == True:
                 voice = await self.bot.join_voice_channel(voice_channel)
-                player = voice.create_ffmpeg_player('https://stream.radioharu.pw/owo')
+                player = voice.create_ffmpeg_player('http://play.theendlessweb.com:8000/listen.pls')
                 player.start()
                 await asyncio.sleep(300)
                 await self._disconnect_voice_client(server)
@@ -69,23 +68,17 @@ class RadioHaru:
     @radioharu.command(pass_context=True, no_pm=True)
     @checks.serverowner_or_permissions(manage_server=True)
     async def stop(self, ctx):
-        """Stop Radio Haru"""
+        """Say goodbye to your radio :("""
         server = ctx.message.server
         author = ctx.message.author
         await self._disconnect_voice_client(server)
         voice_channel = author.voice_channel
         voice = await self.bot.join_voice_channel(voice_channel)
-        player = voice.create_ffmpeg_player('https://cdn.discordapp.com/attachments/336598653923753987/360425539309142037/radioharu_goodbye.mp3')
         player.start()
         await asyncio.sleep(1)
         await self._disconnect_voice_client(server)
-        await self.bot.say(":red_circle: **Stopped playing Radio!**")
-        
-    @radioharu.command(pass_context=True, no_pm=True)
-    async def donate(self, ctx):
-        """Donate for server costs"""
-        await self.bot.say("**https://patreon.com/harukigrande/**")
-        
+        await self.bot.say(":red_circle: **GOODBYE!**")
+       
     def voice_client(self, server):
         return self.bot.voice_client_in(server)
 
