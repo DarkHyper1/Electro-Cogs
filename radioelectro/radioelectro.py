dbc042ed6c3e83432b5bc6a567b1398aea68839f
Electromaster232 <25728719+Electromaster232@users.noreply.github.com>
2017-10-28 13:25:00 -0400

    Update radioelectro.py

--- radioelectro/radioelectro.py
+++ radioelectro/radioelectro.py
@@ -62,6 +62,8 @@ class RadioElectro:
         """Get Now Playing Song"""
         ip = IcyParser()
         ip.getIcyInformation("http://play.theendlessweb.com:8000/stream")
+        ip.getIcyInformation("http://play.theendlessweb.com:8000/stream")
+        ip.getIcyInformation("http://play.theendlessweb.com:8000/stream")
         await self.bot.say(ip.icy_streamtitle)
     
         
